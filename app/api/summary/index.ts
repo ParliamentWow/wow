62e5fe558a28ece1519a9f8dfaf2d85e4b426a8e
Ankcorn <thomasankcorn@gmail.com>
2024-10-05 22:37:48 +0100

    stuff

--- app/api/summary/index.ts
+++ app/api/summary/index.ts
@@ -3,27 +3,31 @@ import { Hono } from "hono";
 import { z } from "zod";
 
 import { Env } from "~/server";
+import { summaryPrompt } from "../ai/prompts";
+import { SummarySize } from "../ai/types";
 
-const documents = new Hono<{ Bindings: Env }>();
+const summary = new Hono<{ Bindings: Env }>();
 
-documents.post(
-  "/summary/:sessionId",
+summary.post(
+  "/summary",
   zValidator(
     "json",
     z.object({
-      question: z.string(),
+      session: z.string(),
+      billName: z.string(),
     })
   ),
   async (c) => {
     const data = await c.req.valid("json");
-    const sessionId = c.req.param("sessionId");
+    const sessionId = data.session;
     const response = await c.env.AI.run("@cf/baai/bge-large-en-v1.5", {
-      text: data.question,
+      text: data.billName,
     });
 
     if (!c.env.TURBOPUFFER_KEY) {
       throw new Error("TURBOPUFFER_KEY not found");
     }
+
     const pufResponse = await fetch(
       `https://api.turbopuffer.com/v1/vectors/${sessionId}/query`,
       {
@@ -35,23 +39,46 @@ documents.post(
         body: JSON.stringify({
           vector: response.data[0],
           //   rank_by: ["page_content", "BM25", data.question],
-          top_k: 4,
+          top_k: 10,
           include_attributes: true,
         }),
       }
     );
 
+    console.log("PUFF DONE");
     if (!pufResponse.ok) {
-      throw new Error("Failed to insert into puffer");
+      const txt = await pufResponse.text();
+      console.log(txt);
+      throw new Error("PUFF request failed");
     }
+
+    const puffData = (await pufResponse.json()) as {
+      attributes: {
+        page_content: string;
+      };
+    }[];
+    const prompt = summaryPrompt(
+      SummarySize.Short,
+      puffData
+        .map((pd) => `<content>${pd.attributes.page_content}</content>`)
+        .join("\n"),
+      data.billName
+    );
+    const mistral = await c.env.AI.run(
+      "@cf/mistral/mistral-7b-instruct-v0.2-lora",
+      {
+        prompt,
+      }
+    );
+
     return c.json(
       {
         message: "Summary fetched",
-        result: await pufResponse.json(),
+        result: mistral,
       },
       201
     );
   }
 );
 
-export default documents;
+export default summary;
