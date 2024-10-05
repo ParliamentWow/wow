01345c0f62129a6dfebc541efce5544c84889e6f
Sunil Pai <spai@cloudflare.com>
2024-10-05 12:32:42 +0100

    remix app

--- app/api/transcriptions/index.ts
+++ app/api/transcriptions/index.ts
@@ -1,13 +1,13 @@
-import { Hono } from "hono";
-import { getD1Client } from "../../data";
-import { Env } from "~/server";
 import { eq } from "drizzle-orm";
-import { Transcription, transcriptionDB } from "~/data/schema";
+import { Hono } from "hono";
+import { type Transcription, transcriptionDB } from "~/data/schema";
+import type { Env } from "~/server";
+import { getDBClient } from "../../data";
 
 const transcriptions = new Hono<{ Bindings: Env }>();
 
 transcriptions.get("/transcriptions", async (c) => {
-  const db = getD1Client(c.env);
+  const db = getDBClient(c.env);
   const transcriptions = await db.query.transcriptionDB.findMany();
 
   return c.json(
@@ -21,7 +21,7 @@ transcriptions.get("/transcriptions", async (c) => {
 
 transcriptions.get("/transcriptions/:id", async (c) => {
   const id = c.req.param("id");
-  const db = getD1Client(c.env);
+  const db = getDBClient(c.env);
   const transcription = await db.query.transcriptionDB.findFirst({
     where: eq(transcriptionDB.id, id),
   });
@@ -36,16 +36,15 @@ transcriptions.get("/transcriptions/:id", async (c) => {
 });
 
 transcriptions.post("/transcriptions", async (c) => {
-
-    const data = await c.req.json() as Transcription;
-    const db = getD1Client(c.env);
-    await db.insert(transcriptionDB).values(data);
-    return c.json(
-        {
-        message: "Transcription created",
-        },
-        201
-    );
+  const data = (await c.req.json()) as Transcription;
+  const db = getDBClient(c.env);
+  await db.insert(transcriptionDB).values(data);
+  return c.json(
+    {
+      message: "Transcription created",
+    },
+    201
+  );
 });
 
 export default transcriptions;
