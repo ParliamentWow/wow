e963a177701bd7dd8fc91888c1ace025c0bd0722
Ankcorn <thomasankcorn@gmail.com>
2024-10-05 22:50:48 +0100

    question endpoint

--- app/api/summary/index.ts
+++ app/api/summary/index.ts
@@ -80,4 +80,130 @@ summary.post(
   }
 );
 
+summary.post(
+  "/question",
+  zValidator(
+    "json",
+    z.object({
+      sessionId: z.string(),
+      billName: z.string(),
+      question: z.string(),
+    })
+  ),
+  async (c) => {
+    const data = await c.req.valid("json");
+    const sessionId = data.sessionId;
+
+    const transcriptionEmbedding = await c.env.AI.run(
+      "@cf/baai/bge-large-en-v1.5",
+      {
+        text: data.billName,
+      }
+    );
+
+    const billEmbedding = await c.env.AI.run("@cf/baai/bge-large-en-v1.5", {
+      text: data.billName,
+    });
+
+    if (!c.env.TURBOPUFFER_KEY) {
+      throw new Error("TURBOPUFFER_KEY not found");
+    }
+
+    const pufResponse = await fetch(
+      `https://api.turbopuffer.com/v1/vectors/${sessionId}/query`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${c.env.TURBOPUFFER_KEY}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          vector: transcriptionEmbedding.data[0],
+          //   rank_by: ["page_content", "BM25", data.question],
+          top_k: 10,
+          include_attributes: true,
+        }),
+      }
+    );
+
+    const pufBillsResponse = await fetch(
+      `https://api.turbopuffer.com/v1/vectors/bills/query`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${c.env.TURBOPUFFER_KEY}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          vector: billEmbedding.data[0],
+          //   rank_by: ["page_content", "BM25", data.question],
+          top_k: 10,
+          include_attributes: true,
+        }),
+      }
+    );
+
+    if (!pufResponse.ok || !pufBillsResponse.ok) {
+      const txt = await pufResponse.text();
+      const txt2 = await pufBillsResponse.text();
+      console.log(txt, txt2);
+      throw new Error("PUFF request failed");
+    }
+
+    const puffData = (await pufResponse.json()) as {
+      attributes: {
+        page_content: string;
+      };
+    }[];
+
+    const puffBillsData = (await pufBillsResponse.json()) as {
+      attributes: {
+        title: string;
+        description: string;
+        url: string;
+        content: string;
+        publishDate: string;
+      };
+    }[];
+
+    const bill = data.billName;
+
+    const billExtracts = puffBillsData
+      .map(
+        (pd) =>
+          `<title>${pd.attributes.title}</title> <description>${pd.attributes.description}</description> <url>${pd.attributes.url}</url> <content>${pd.attributes.content}</content>`
+      )
+      .join("\n");
+
+    const transcripts = summaryPrompt(
+      SummarySize.Short,
+      puffData
+        .map((pd) => `<content>${pd.attributes.page_content}</content>`)
+        .join("\n"),
+      data.billName
+    );
+
+    const prompt = `
+        ${question}
+        ${bill}
+        ${billExtracts}
+        ${transcripts}
+    `;
+    const mistral = await c.env.AI.run(
+      "@cf/mistral/mistral-7b-instruct-v0.2-lora",
+      {
+        prompt,
+      }
+    );
+
+    return c.json(
+      {
+        message: "Summary fetched",
+        result: mistral,
+      },
+      201
+    );
+  }
+);
+
 export default summary;
