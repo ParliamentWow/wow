2c43b2be4bfa3f84638557916f2605b62b6c7c35
Ankcorn <thomasankcorn@gmail.com>
2024-10-05 12:05:50 +0100

    add sessions

--- app/data/index.ts
+++ app/data/index.ts
@@ -2,7 +2,7 @@ import { Env } from "~/server";
 import * as schema from "./schema";
 import { drizzle } from "drizzle-orm/d1";
 
-export const getDBClient = (env: Env) => {
+export const getD1Client = (env: Env) => {
     return drizzle(env.DB, {
         schema
     });
--- app/data/schema.ts
+++ app/data/schema.ts
@@ -1,16 +1,37 @@
-import { sql } from "drizzle-orm";
+import { relations, sql } from "drizzle-orm";
 import { text, integer, sqliteTable } from "drizzle-orm/sqlite-core";
 
 
 export type Transcription = {
     id: string,
     content: string,
+    sessionId: string,
     timestamp: Date
 }
 
+export const sessionDB = sqliteTable('session', {
+    id: text('id').primaryKey(),
+    name: text('name').notNull(),
+    description: text('description'),
+    vidioUrl: text('vidioUrl').notNull(),
+    timestamp: integer('timestamp', { mode: 'timestamp' }).notNull(),
+});
+
+export const sessionRelations = relations(sessionDB, ({ many }) => ({
+    transcriptions: many(transcriptionDB),
+}));
+  
 
 export const transcriptionDB = sqliteTable('transcriptions', {
   id: text('id').primaryKey(),
+  sessionId: text('sessionId').notNull(),
   content: text('content').notNull(),
   timestamp: integer('timestamp', { mode: 'timestamp' }).notNull(),
 });
+
+export const transcriptionRelations = relations(transcriptionDB, ({ one }) => ({
+    session: one(sessionDB, {
+        fields: [transcriptionDB.sessionId],
+        references: [sessionDB.id],
+    }),
+}));
