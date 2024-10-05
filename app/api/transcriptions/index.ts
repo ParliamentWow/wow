0b16d6a0822fc2cd907fd84948d1a1cc50e5346f
Ankcorn <thomasankcorn@gmail.com>
2024-10-05 16:58:36 +0100

    add vector db

--- app/api/transcriptions/index.ts
+++ app/api/transcriptions/index.ts
@@ -23,6 +23,7 @@ transcriptions.get("/transcriptions", async (c) => {
 transcriptions.get("/transcriptions/:id", async (c) => {
   const id = c.req.param("id");
   const db = getD1Client(c.env);
+
   const transcription = await db.query.transcriptionDB.findFirst({
     where: eq(transcriptionDB.id, id),
   });
@@ -36,17 +37,68 @@ transcriptions.get("/transcriptions/:id", async (c) => {
   );
 });
 
-transcriptions.post("/transcriptions", zValidator('json', insertTranscriptionSchema), async (c) => {
-  const data = await c.req.valid("json")
-  console.log(data)
-  const db = getD1Client(c.env);
-  await db.insert(transcriptionDB).values(data);
-  return c.json(
-    {
-      message: "Transcription created",
-    },
-    201
-  );
-});
+transcriptions.post(
+  "/transcriptions",
+  zValidator("json", insertTranscriptionSchema),
+  async (c) => {
+    const data = await c.req.valid("json");
+    console.log(data);
+    const db = getD1Client(c.env);
+    await db.insert(transcriptionDB).values(data);
+
+    const response = await c.env.AI.run("@cf/baai/bge-large-en-v1.5", {
+      text: data.content,
+    });
+
+    if (!c.env.TURBOPUFFER_KEY) {
+      throw new Error("TURBOPUFFER_KEY not found");
+    }
+    const pufResponse = await fetch(
+      "https://api.turbopuffer.com/v1/vectors/transcription",
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${c.env.TURBOPUFFER_KEY}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          upserts: [
+            {
+              id: data.id,
+              vector: response.data[0],
+              attributes: {
+                page_content: data.content,
+                metadata: JSON.stringify({ ...data, content: undefined }),
+              },
+            },
+          ],
+          distance_metric: "cosine_distance",
+          schema: {
+            page_content: {
+              type: "string",
+              bm25: {
+                language: "english",
+                stemming: false,
+                remove_stopwords: true,
+                case_sensitive: false,
+              },
+            },
+          },
+        }),
+      }
+    );
+
+    if (!pufResponse.ok) {
+      throw new Error("Failed to insert into puffer");
+    }
+
+    return c.json(
+      {
+        message: "Transcription created",
+      },
+      201
+    );
+  }
+);
 
 export default transcriptions;
--- app/data/schema.ts
+++ app/data/schema.ts
@@ -1,36 +1,32 @@
-import { relations, sql } from "drizzle-orm";
+import { relations } from "drizzle-orm";
 import { text, integer, sqliteTable } from "drizzle-orm/sqlite-core";
 import { createInsertSchema } from "drizzle-zod";
-import { z } from "zod"
 
-export const sessionDB = sqliteTable('session', {
-    id: text('id').primaryKey(),
-    name: text('name').notNull(),
-    description: text('description'),
-    vidioUrl: text('vidioUrl').notNull(),
-    timestamp: integer('timestamp', { mode: 'timestamp' }).notNull(),
+export const sessionDB = sqliteTable("session", {
+  id: text("id").primaryKey(),
+  name: text("name").notNull(),
+  description: text("description"),
+  vidioUrl: text("vidioUrl").notNull(),
+  timestamp: integer("timestamp", { mode: "timestamp" }).notNull(),
 });
 
 export const sessionRelations = relations(sessionDB, ({ many }) => ({
-    transcriptions: many(transcriptionDB),
+  transcriptions: many(transcriptionDB),
 }));
-  
 
-export const transcriptionDB = sqliteTable('transcriptions', {
-  id: text('id').primaryKey(),
-  sessionId: text('sessionId').notNull(), // iso date string
-  content: text('content').notNull(),
-  timestampStart: integer('timestamp_start').notNull(),
-  timestampEnd: integer('timestamp_end').notNull(),
+export const transcriptionDB = sqliteTable("transcriptions", {
+  id: text("id").primaryKey(),
+  sessionId: text("sessionId").notNull(), // iso date string
+  content: text("content").notNull(),
+  timestampStart: integer("timestamp_start").notNull(),
+  timestampEnd: integer("timestamp_end").notNull(),
 });
 
 export const insertTranscriptionSchema = createInsertSchema(transcriptionDB);
 
-
 export const transcriptionRelations = relations(transcriptionDB, ({ one }) => ({
-    session: one(sessionDB, {
-        fields: [transcriptionDB.sessionId],
-        references: [sessionDB.id],
-    }),
+  session: one(sessionDB, {
+    fields: [transcriptionDB.sessionId],
+    references: [sessionDB.id],
+  }),
 }));
-
